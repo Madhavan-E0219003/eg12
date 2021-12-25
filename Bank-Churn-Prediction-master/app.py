8dae28497aa0dc0337ba2402ddc63c278c82a7fa
Madhavan-E0219003 <78415653+Madhavan-E0219003@users.noreply.github.com>
2021-12-25 15:19:32 +0530

    Update app.py

--- Bank-Churn-Prediction-master/app.py
+++ Bank-Churn-Prediction-master/app.py
@@ -8,22 +8,13 @@ app = Flask(__name__)
 
 # Load saved models
 dt_model = joblib.load('models/nate_decision_tree.sav')
-# dl_model = joblib.load('models/imblearn_pipeline.sav')
-# dl_model.named_steps['kerasclassifier'].model = load_model('models/keras_model.h5')
-knn_model = joblib.load('models/nate_knn.sav')
-lr_model = joblib.load('models/nate_logistic_regression.sav')
-rf_model = joblib.load('models/nate_random_forest.sav')
-svm_model = joblib.load('models/SVM_model.sav')
+
 
 
 # Dictionary of all loaded models
 loaded_models = {
     'dt': dt_model,
-    #'dl': dl_model,
-    'knn': knn_model,
-    'lr': lr_model,
-    'rf': rf_model,
-    'svm': svm_model
+ 
 
 }
 
@@ -35,12 +26,8 @@ def decode(pred):
 @app.route('/')
 def home():
     # Initial rendering
-    result = [{'model':'Decision Tree', 'prediction':' '},
-              #{'model':'Deep Learning', 'prediction':' '},
-              {'model': 'K-nearest Neighbors', 'prediction': ' '},
-              {'model': 'Logistic Regression', 'prediction': ' '},
-              {'model': 'Random Forest', 'prediction': ' '},
-              {'model': 'SVM', 'prediction': ' '}
+    result = [{'model':'ExtraTree Classifier', 'prediction':' '}
+         
 ]
     
     # Create main dictionary
@@ -91,12 +78,8 @@ def predict():
         predl.append(decode(m.predict(new_array)[0]))
 
     result = [
-            {'model':'Decision Tree', 'prediction':predl[0]},
-            #{'model':'Deep Learning', 'prediction':predl[1]},
-            {'model': 'K-nearest Neighbors', 'prediction': predl[1]},
-            {'model': 'Logistic Regression', 'prediction': predl[2]},
-            {'model': 'Random Forest', 'prediction': predl[3]},
-            {'model': 'SVM', 'prediction': predl[4]}
+            {'model':'ExtraTree Classifier', 'prediction':predl[0]}
+            
            
             ]
 
